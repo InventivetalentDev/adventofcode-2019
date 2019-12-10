cddd43f90bcf6ccbad690de340e648c9b5d8b46c
inventivetalent <mail@inventivetalent.org>
2019-12-10 11:33:16 +0100

    day 10 part two

---
+++ day10_two.js
@@ -0,0 +1,152 @@
+let grid = [[0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0], [0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1], [1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0], [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1], [0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1], [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1], [0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1], [1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1], [0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0], [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0], [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0], [0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1], [1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0]];
+let totalAsteroids = 325;
+
+// Location of station from the previous part
+let x = 23;
+let y = 19;
+
+
+function angleBetweenTwoPoints(a, b) {
+    return Math.atan2(b[1] - a[1], b[0] - a[0]) * 180 / Math.PI;
+}
+
+function distanceBetweenTwoPoints(a, b) {
+    return Math.sqrt(Math.pow((a[0] - b[0]), 2) + Math.pow((a[1] - b[1]), 2))
+}
+
+function removeFromArray(arr, v) {
+    let i = arr.indexOf(v);
+    if (i > -1) {
+        arr.splice(i, 1);
+    }
+}
+
+function getLinesOfSight(x, y) {
+    let anglesAndDistances = {};
+    let canSee = [];
+
+    for (let y1 = 0; y1 < grid.length; y1++) {
+        for (let x1 = 0; x1 < grid[y1].length; x1++) {
+            if (grid[y1][x1] === 1) {
+                let angle = angleBetweenTwoPoints([x, y], [x1, y1]);
+                let distance = distanceBetweenTwoPoints([x, y], [x1, y1]);
+
+                anglesAndDistances[x1 + "," + y1] = {
+                    angle: angle,
+                    distance: distance
+                };
+                canSee.push(x1 + "," + y1)
+            }
+        }
+    }
+
+    let c = 1;
+    while (c > 0) {
+        for (let k in anglesAndDistances) {
+            let angleAndDistance = anglesAndDistances[k];
+            for (let k1 in anglesAndDistances) {
+                let otherAngleAndDistance = anglesAndDistances[k1];
+
+                if (angleAndDistance.angle === otherAngleAndDistance.angle) {
+                    if (angleAndDistance.distance < otherAngleAndDistance.distance) {
+                        removeFromArray(canSee, k1);// remove other
+                        c++;
+                    } else if (otherAngleAndDistance.distance < angleAndDistance.distance) {
+                        removeFromArray(canSee, k);// remove first
+                        c++;
+                    } else {
+                        c++;
+                        // console.warn(k + " and " + k1 + " have same angle and distance from " + x + "," + y + "! ("+angleAndDistance.angle+","+angleAndDistance.distance+") Panic!");
+                    }
+                }else{
+                    c--;
+                }
+            }
+        }
+    }
+
+    return canSee;
+
+}
+
+let anglesAndDistances = [];
+for (let y1 = 0; y1 < grid.length; y1++) {
+    for (let x1 = 0; x1 < grid[y1].length; x1++) {
+        if (grid[y1][x1] === 1) {
+            let angle = angleBetweenTwoPoints([x, y], [x1, y1]);
+            let distance = distanceBetweenTwoPoints([x, y], [x1, y1]);
+
+            anglesAndDistances.push({
+                key: x1 + "," + y1,
+                x: x1,
+                y: y1,
+                angle: angle,
+                distance: distance
+            });
+        }
+    }
+}
+anglesAndDistances.sort((a, b) => (a.distance > b.distance) ? 1 : ((b.distance > a.distance) ? -1 : 0));
+console.log(anglesAndDistances);
+
+function similarAngle(a, b) {
+    return Math.abs(a) - Math.abs(b) < (1 / 360);
+}
+
+let lastHit ;
+
+function printGrid() {
+    for (let y1 = 0; y1 < grid.length; y1++) {
+        let line = "";
+        for (let x1 = 0; x1 < grid[y1].length; x1++) {
+            if (x === x1 && y === y1) {
+                line += "X"
+            } else if (grid[y1][x1] === 1) {
+                line += "#"
+            }else if(lastHit&&lastHit[0]===x1&&lastHit[1]===y1){
+                line += "$";
+            } else {
+                line += "."
+            }
+        }
+        console.log(line);
+    }
+    console.log("");
+}
+
+let c = 0;
+let canSee = getLinesOfSight(x, y);
+let d = 0;
+let incr = 1 ;
+while (c < totalAsteroids) {
+    for (let i = 0; i < anglesAndDistances.length; i++) {
+        let angleAndDistance = anglesAndDistances[i];
+        if (angleAndDistance.shot) continue;
+        if (canSee.indexOf(angleAndDistance.key) === -1) continue;
+        if (similarAngle(angleAndDistance.angle, d)) {
+            angleAndDistance.shot = true;
+            grid[angleAndDistance.y][angleAndDistance.x] = 0;
+            lastHit = [angleAndDistance.x, angleAndDistance.y];
+            console.log("#" + (c + 1) + ": " + angleAndDistance.key);
+            canSee = getLinesOfSight(x, y);
+            c++;
+            printGrid()
+            break;
+        }
+    }
+
+    d += incr;
+    if (d > 360) {
+        d -= 360;
+    }
+}
+
+// 1107 too low
+// 1131 too low
+// 1030 too low
+// 714 too low
+
+// 1714 ??? was the right answer apparently, just swapped (1417)
+
+// 2319 too high
+// 2835 too high
